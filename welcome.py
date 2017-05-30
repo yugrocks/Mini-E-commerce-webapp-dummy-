2372c5132dbe408b168ec7f3c9f40dfff6e089c1
Yugal Sharma <yugalsharma13@gmail.com>
2017-05-30 15:33:56 +0530

    Delete welcome.py

--- welcome.py
+++
@@ -1,6 +0,0 @@
-#Nice to see you.
-#It's nothing here in this file.
-#This was made to waste your precious time.
-#Just kidding. It was made to tell github language detector that this project is in python language.
-#Now get out of here and visit directory yugswebsite. It has the content.
-print("SayoNara")
